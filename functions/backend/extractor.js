af6cbd9ae034c1bc9b58cc349886dce3c22460b7
Charles-Eugene Loubao <charlesloubao95@gmail.com>
2019-10-05 09:10:15 -0400

    Faster link processing using Mercury Parser.

--- functions/backend/extractor.js
+++ functions/backend/extractor.js
@@ -1,4 +1,6 @@
 const URL = require('url').URL;
+const Mercury = require('@postlight/mercury-parser');
+const fetch = require('node-fetch');
 
 const getBrowser = require('./browser')
 
@@ -13,159 +15,59 @@ function parseURL(url) {
 }
 
 async function getInfo(url) {
-    const blockedResources = [
-        "image",
-        "media",
-        "font",
-    ]
-    
+
     var parsedURL = parseURL(url);
 
     if (!parsedURL || !parsedURL.protocol.startsWith('http')) {
         return { error: 'invalid_url' };
     }
 
-    var page = ""
-    var browser = ""
-    var response = ""
-    try {
-        browser = await getBrowser();
+    const response = await fetch(url, {
+        method: 'HEAD'
+    })
 
-        page = await browser.newPage();
-        await page.setCacheEnabled(false);
+    var type = '';
+    var title = '';
+    var description = '';
+    var hostname = parsedURL.hostname;
+    var preview = '';
 
-        await page.setRequestInterception(true);
-        page.on('request', (request) => {
-            if (blockedResources.indexOf(request.resourceType()) != -1) {
-                request.abort()
-            }
-            else {
-                request.continue()
-            }
+    const contentType = response.headers.get('content-type')
+
+    if (contentType.startsWith("text")) {
+        const parsed = await Mercury.parse(url, {
+            contentType: 'text'
         })
 
-        response = await page.goto(url, {
-            timeout: 25000,
-            waitUntil: 'domcontentloaded'
-        });
-
-        if (response.ok()) {
-            await page.waitFor(2000);
-            const pageTitle = await page.title()
-            const contentType = (response.headers()['content-type'] || response.request().resourceType());
-
-            var type = '';
-            var title = '';
-            var description = '';
-            var hostname = parsedURL.hostname;
-            var preview = '';
-
-            if (contentType.startsWith('text') || contentType == "document") {
-                let evaluation = (await page.evaluate(() => {
-                    let metaArray = []
-
-                    metaArray = metaArray.concat(Array.from(document.querySelectorAll("head > meta[property]")).map(meta => {
-                        let property = meta.attributes.getNamedItem('property').value
-                        let value = meta.content
-                        return {
-                            property, value
-                        }
-                    }))
-                        .concat(Array.from(document.querySelectorAll("head > meta[name]")).map(meta => {
-                            let property = meta.attributes.getNamedItem('name').value
-                            let value = meta.content
-                            return {
-                                property, value
-                            }
-                        }))
-                        .concat(Array.from(document.querySelectorAll("head > title")).map(titleTag => {
-                            return {
-                                property: 'htmlTitle',
-                                value: titleTag.textContent
-                            }
-                        }))
-                        .concat(Array.from(document.querySelectorAll('link[rel=icon]')).map((link) => {
-                            let property = 'favicon'
-                            let value = link.href
-
-                            return {
-                                property, value
-                            }
-                        }))
-
-
-                    return {
-                        metaArray,
-                    }
-                }));
-
-                let meta = {};
-
-                evaluation.metaArray.forEach(entry => {
-                    meta[entry.property] = entry.value
-                })
-
-                title = meta['og:title'] || meta['twitter:title'] || meta['title'] || meta['htmlTitle'] || pageTitle || url
-                description = meta['og:description'] || meta['twitter:description']
-                preview = meta["og:image"] || meta["twitter:image"] || ''
-                type = meta['og:type'] || contentType;
-
-                if (type == "text/html") {
-                    type = "url"
-                }
-
-            }
-            else if (contentType.startsWith("image")) {
-                title = pageTitle
-                description = ""
-                preview = url
-                type = "image"
-            }
-            else {
-                console.log(`ContentType ${contentType} cannot be parsed`);
-                title = url;
-                description = url;
-                type = contentType
-            }
-
-            if (preview && !preview.startsWith("http")) {
-                preview = 'http://' + hostname + (preview.startsWith("/") ? preview : "/" + preview)
-            }
-
-            return {
-                url,
-                data: {
-                    title,
-                    description,
-                    preview,
-                    hostname,
-                    type
-                }
-            };
-        }
-        else {
-            console.error(`Error occurred: ${response.status()}`);
-            return {
-                error: response.status()
-            };
-        }
+        title = parsed.title
+        description = parsed.excerpt
+        preview = parsed.lead_image_url
+        type = 'website'
     }
-    catch (error) {
-        console.error(error)
-        return {
-            url: url,
-            data: {
-                title: url,
-                hostname: parsedURL.hostname
-            }
-        }
+    else if (contentType.startsWith("image")) {
+        title = url.substring(url.lastIndexOf('/') + 1)
+        description = ""
+        preview = url
+        type = "image"
     }
-    finally {
-        console.log('Closing page');
-        if (page) {
-            page.close();
-        }
+    else {
+        console.log(`ContentType ${contentType} cannot be parsed`);
+        title = url;
+        description = url;
+        type = contentType
     }
+
+    return {
+        url,
+        data: {
+            title,
+            description,
+            preview,
+            hostname,
+            type
+        }
+    };
+
 }
 
 async function getScreenshot(url) {
