3dfed83dd650cf7231304b68c4fe63c00834f974
Charles-Eugene Loubao <charlesloubao95@gmail.com>
2019-10-06 16:53:55 -0400

    Added error reporting

--- src/main.js
+++ src/main.js
@@ -11,6 +11,32 @@ import store from './store'
 
 import FirebasePlugin from './plugins/FirebasePlugin'
 
+import * as Sentry from '@sentry/browser';
+import * as Integrations from '@sentry/integrations';
+
+Sentry.init({
+  dsn: 'https://9418e607dada4adba467367c22ee95e9@sentry.io/1771484',
+  integrations: [new Integrations.Vue({ Vue, attachProps: true, logErrors: true })],
+  beforeSend(event, hint) {
+    // Check if it is an exception, and if so, show the report dialog
+    if (event.exception) {
+      var name = "";
+      var email = ""
+
+      if (firebase.auth().currentUser) {
+        name = firebase.auth().currentUser.displayName
+        email = firebase.auth().currentUser.email
+      }
+      Sentry.showReportDialog({
+        eventId: event.event_id, user: {
+          name, email
+        }
+      });
+    }
+    return event;
+  }
+});
+
 firebase.initializeApp({
   apiKey: "AIzaSyDNHOidgoTpuTVv4GJA6xeNPXOSzxJ7eBw",
   authDomain: "moodboard-mvp.firebaseapp.com",
@@ -22,8 +48,18 @@ firebase.initializeApp({
   measurementId: "G-C0MC8SNSLK"
 });
 
-const unsubscribe = firebase.auth().onAuthStateChanged(() => {
+const unsubscribe = firebase.auth().onAuthStateChanged((auth) => {
   unsubscribe()
+
+  if (auth) {
+    Sentry.configureScope(scope => {
+      scope.setUser({
+        email: auth.email,
+        username: auth.displayName,
+        id: auth.uid
+      })
+    })
+  }
 
   Vue.use(FirebasePlugin)
 
